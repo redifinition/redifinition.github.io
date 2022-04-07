91505e3c00d3e56a908f712c5d9e0f85f8fa7129
RedefinitionLQ <19921305202@163.com>
2022-04-07 11:26:07 +0800

    Site updated: 2022-04-07 11:26:06

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "3",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "1",
+        "language": "zh",
+        "borderRadius": "10",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "270",
+        "top": "10",
+        "key": "142937cb53a14032899ac96f18e8830c"
+    }
+}
